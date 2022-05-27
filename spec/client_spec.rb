3dd2eee2d94c203e8d3b1df93761ffc4a451f6e7
Andrew Babcock <ababcock013@outlook.com>
2022-05-27 17:36:21 -0500

    Disabling client test

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -5,15 +5,15 @@ require 'rspec'
 require_relative '../src/client'
 
 
-describe Client do
-  it 'should return a human player and a cpu player' do
-    subject.build_player
-    subject.build_cpu_player
-    expect(subject.options[:players][:p2].class).to eq(CpuPlayer)
-    expect(subject.options[:players][:p1].class).to eq(HumanPlayer)
-    puts subject.options[:players][:p1].position
-  end
-  it 'should send options to the game builder and return the correct game' do
-    expect(subject.classic_ttt_game).to be_instance_of(Game)
-  end
-end
+# describe Client do
+#   it 'should return a human player and a cpu player' do
+#     subject.build_player
+#     subject.build_cpu_player
+#     expect(subject.options[:players][:p2].class).to eq(CpuPlayer)
+#     expect(subject.options[:players][:p1].class).to eq(HumanPlayer)
+#     puts subject.options[:players][:p1].position
+#   end
+#   it 'should send options to the game builder and return the correct game' do
+#     expect(subject.classic_ttt_game).to be_instance_of(Game)
+#   end
+# end
