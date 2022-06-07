fbce4460f1b8beede6d47e57ae63bab6cd08cb73
Andrew Babcock <ababcock013@outlook.com>
2022-06-07 13:45:29 -0500

    Fixed test for board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,18 +4,18 @@ require 'rspec'
 require_relative '../src/ttt_board'
 
 describe TttBoard do
-  it 'should initialize board with blank spaces' do
+  it 'should initialize board with numbered spaces' do
     expect(subject.board_spaces).to eq({ 0 => '00', 1 => '01', 2 => '02', 3 => '03', 4 => '04', 5 => '05', 6 => '06',
                                          7 => '07', 8 => '08' })
   end
 
-  it 'square 7 should be string with space " "' do
+  it 'square 7 should be string with "07"' do
     expect(subject.board_spaces[7]).to eq('07')
   end
 
   context Board do
     let(:board) { described_class.new(5) }
-    it 'should have a size of 9' do
+    it ' when user inputs a 5, user should get a board size of 25 spaces' do
       expect(board.board_spaces.length).to be(25)
     end
   end
